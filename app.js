3075517b5855e96f85cce5473e67ae1d3a2635f0
Breyana Scales <breyanaj@gmail.com>
2017-08-11 14:35:53 -0700

    Connect route to index.pug

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.set('view engine', 'pug')
+
+app.get('/form-get', (request, response) => {
+  response.render('index', { method: 'GET' })
+})
+
 app.listen(3000, () => {
   console.log('App is now listening on 3000')
 })
