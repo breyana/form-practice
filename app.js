d6c35c3def89802e2c4ac839bf82867e6eb0ac9c
Breyana Scales <breyanaj@gmail.com>
2017-08-15 11:38:53 -0700

    Add get and post routes and submit form

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.set('view engine', 'pug')
 
 app.get('/form-get', (request, response) => {
-  response.render('index', { method: 'GET' })
+  response.render('form', { method: 'GET' })
+})
+
+app.get('/form-post', (request, response) => {
+  response.render('form', { method: 'POST' })
+})
+
+app.all('/submit-form', (request, response) => {
+  response.json({ "body-params": request.body, "query-params": request.query})
 })
 
 app.listen(3000, () => {
